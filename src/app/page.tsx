31708dbdce86429e7aa3645ef9b425e0070b6775
Breno L. Lopes <brernolopes@hotmail.com>
2024-10-23 13:54:28 -0300

    feat: :sparkles: Implementando catalogo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+'use-client'
 import Carrossel from "./component/client-page/Carrossel";
+import Catalog from "./component/client-page/Catalog";
 import { NavBar } from "./component/client-page/NavBar";
 
 export default function Home() {
+
   return (
     <div>
       <NavBar />
       <Carrossel />
+      <Catalog />
+      <Catalog />
     </div>
   );
 }
