7995eb4b17e87aff58bd027146812a795bfbe8d0
Breno L. Lopes <brernolopes@hotmail.com>
2024-11-12 11:31:16 -0300

    feat: :sparkles: finalizando componente about

--- src/app/component/client-page/About.tsx
+++ src/app/component/client-page/About.tsx
@@ -1,7 +1,13 @@
 export default function About() {
   return(
-    <div>
-      
+    <div className="text-center w-3/4 mx-auto flex items-center justify-center flex-wrap">
+      <h1 className="text-xl font-bold">O que é topográfia</h1>
+      <p>
+        A topografia é a ciência que estuda e descreve as características da superfície terrestre,
+        incluindo a forma e o relevo de uma região. Ela envolve a medição e representação detalhada de terrenos,
+        suas elevações, declives, depressões e outras formas de relevo,
+        geralmente com o objetivo de criar mapas ou plantas que descrevem essas feições.
+      </p>
     </div>
   )
 };
