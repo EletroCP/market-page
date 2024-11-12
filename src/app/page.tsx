310cc2e89081ede4d61738e9b15f10c1911472a2
Breno L. Lopes <brernolopes@hotmail.com>
2024-11-12 11:20:50 -0300

    feat: :sparkles: aplicando formulario com lazy loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,32 @@
-'use-client'
+'use-client';
+import React, { Suspense } from 'react';
 import Carrossel from "./component/client-page/Carrossel";
 import Catalog from "./component/client-page/Catalog";
 import { NavBar } from "./component/client-page/NavBar";
 
-export default function Home() {
+/*
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
+const SearchForService = React.lazy(() => 
+  delay(13000).then(() => import("./component/client-page/SearchForServise"))
+);
+*/
+
+const SearchForService = React.lazy(() => import("./component/client-page/SearchForServise"));
+
+
+export default function Home() {
   return (
     <div>
       <NavBar />
       <Carrossel />
       <Catalog />
       <Catalog />
+      <Suspense fallback={<div>Loading...</div>}>
+        <SearchForService />
+      </Suspense>
     </div>
   );
 }
